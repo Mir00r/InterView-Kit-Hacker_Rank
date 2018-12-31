bb6aed1b6f1c09c93dc6276bac4b48612ea16fb8
Mir00r <abdur.razzaq06@gmail.com>
2018-12-31 14:47:00 +0600

    Warp-up
    This is from warm-up section problem

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
