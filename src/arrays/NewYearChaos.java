f16931c18b58b9a4a71c2c4c41233a0ea07049cd
Mir00r <abdur.razzaq06@gmail.com>
2019-01-13 20:57:50 +0600

    It's New Year's Day and everyone's in line for the Wonderland rollercoaster ride! There are a number of people queued up, and each person wears a sticker indicating their initial position in the queue. Initial positions increment by  from  at the front of the line to  at the back. Any person in the queue can bribe the person directly in front of them to swap positions. If two people swap positions, they still wear the same sticker denoting their original places in line. One person can bribe at most two others. For example, if and  bribes , the queue will look like this: . Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took place to get the queue into its current state!

---
+++ src/arrays/NewYearChaos.java
@@ -0,0 +1,53 @@
+package arrays;
+
+import java.util.Scanner;
+
+/**
+ * Since the question only asks the number of bribes, there's no need to really do a sorting,
+ * not element swapping, not "bubbling up" an element.
+ * All you need to do is to count the number of people who overtake a person.
+ *
+ * @author Mir00r on 12-Jan-19
+ */
+public class NewYearChaos {
+
+    static void minimumBribes(int[] person) {
+        int bribe = 0, maxN;
+        int len = person.length;
+        for (int i = 0; i < len; i++) {
+            if (person[i] - (i + 1) > 2) {
+                System.out.println("Too chaotic");
+                break;
+            }
+            maxN = Math.max(0, person[i] - 2);
+            for (int j = maxN; j < i; j++) {
+                if (person[j] > person[i]) bribe++;
+            }
+        }
+        if (bribe > 0) System.out.println(bribe);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+
+            }
+            minimumBribes(q);
+        }
+        scanner.close();
+    }
+}
